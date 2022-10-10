196cb966598c75773a6a7aa5e6e206a5d39717ec
Vbasov <0120912@mail.ru>
2022-10-10 09:32:50 +0300

    add 01

---
+++ 01/dz.py
@@ -0,0 +1,7 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв""
+
+t = "абв"
+text = "абв лучше, чем абвгд"
+lst = text.split()
+new_lst = [ x for x in lst if t not in x ]
+print(new_lst)
